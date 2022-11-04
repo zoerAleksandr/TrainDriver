d6e910e5ac0f7cb7f14e435784de611397ae70ba
Aleksandr <79718551+zoerAleksandr@users.noreply.github.com>
2022-11-04 15:07:44 +0300

    Добавил зависимости constraint compose, загрузил флаги России, Беларуси, Казахстана. Добавил флаг в поле ввода номера телефона

--- app/src/main/java/com/example/traindriver/ui/element_screen/general.kt
+++ app/src/main/java/com/example/traindriver/ui/element_screen/general.kt
@@ -1,11 +1,10 @@
 package com.example.traindriver.ui.element_screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -13,22 +12,30 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.traindriver.R
 import com.example.traindriver.ui.theme.ShapeBackground
+import com.example.traindriver.ui.theme.TrainDriverTheme
 import com.example.traindriver.ui.theme.Typography
 import com.example.traindriver.ui.theme.overpassFontFamily
+import com.example.traindriver.ui.util.DarkLightPreviews
 import com.example.traindriver.ui.util.FieldIsFilled
+import com.example.traindriver.ui.util.FontScalePreviews
+import com.example.traindriver.ui.util.LocaleState
 
 @Composable
 fun CustomTextField(
@@ -38,7 +45,7 @@ fun CustomTextField(
     maxLength: Int,
     isFilledCallback: FieldIsFilled? = null
 ) {
-    BasicTextField(
+    ConstraintLayout(
         modifier = Modifier
             .fillMaxWidth()
             .height(dimensionResource(id = R.dimen.min_size_view))
@@ -50,43 +57,90 @@ fun CustomTextField(
                 color = MaterialTheme.colors.onSurface,
                 width = dimensionResource(id = R.dimen.width_border_input_field),
                 shape = ShapeBackground.small
-            ),
-        value = data.value,
-        onValueChange = {
-            if (it.length <= maxLength) data.value = it
-            isFilledCallback?.isFilled(it.length >= maxLength)
-        },
-        visualTransformation = transformation,
-        textStyle = TextStyle(
-            color = MaterialTheme.colors.onBackground,
-            fontSize = 18.sp,
-            fontFamily = overpassFontFamily,
-            fontWeight = FontWeight.Light
-        ),
-        cursorBrush = SolidColor(MaterialTheme.colors.onSurface),
-        decorationBox = { innerTextField ->
-            Box(
-                modifier = Modifier
-                    .padding(start = dimensionResource(id = R.dimen.padding_start_input_field)),
-                contentAlignment = Alignment.CenterStart
-            ) {
-                if (data.value.isEmpty()) {
-                    Text(
-                        text = placeholderText,
-                        style = Typography.caption,
-                        color = MaterialTheme.colors.onSurface
-                    )
+            )
+    ) {
+        val (button, editText) = createRefs()
+        Box(
+            modifier = Modifier
+                .constrainAs(button) {
+                    start.linkTo(parent.start, margin = 0.dp)
                 }
-                innerTextField()
+                .clickable{
+                    // TODO выпадающий список
+                }
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.russia),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(start = 8.dp, top = 8.dp, bottom = 8.dp)
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.ic_arrow_drop_down_24),
+                    contentDescription = null
+                )
             }
-        },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Phone,
-            imeAction = ImeAction.Go
-        ),
-        keyboardActions = KeyboardActions(
-            // TODO
-        ),
-        singleLine = true
-    )
+        }
+        BasicTextField(
+            modifier = Modifier
+                .fillMaxHeight()
+                .constrainAs(editText) {
+                    start.linkTo(button.end)
+                },
+            value = data.value,
+            onValueChange = {
+                if (it.length <= maxLength) data.value = it
+                isFilledCallback?.isFilled(it.length >= maxLength)
+            },
+            visualTransformation = transformation,
+            textStyle = TextStyle(
+                color = MaterialTheme.colors.onBackground,
+                fontSize = 18.sp,
+                fontFamily = overpassFontFamily,
+                fontWeight = FontWeight.Light
+            ),
+            cursorBrush = SolidColor(MaterialTheme.colors.onSurface),
+            decorationBox = { innerTextField ->
+                Box(
+                    modifier = Modifier
+                        .padding(start = dimensionResource(id = R.dimen.padding_start_input_field)),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    if (data.value.isEmpty()) {
+                        Text(
+                            text = placeholderText,
+                            style = Typography.caption,
+                            color = MaterialTheme.colors.onSurface
+                        )
+                    }
+                    innerTextField()
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Phone,
+                imeAction = ImeAction.Go
+            ),
+            keyboardActions = KeyboardActions(
+                // TODO
+            ),
+            singleLine = true
+        )
+    }
+}
+
+@Composable
+@DarkLightPreviews
+@FontScalePreviews
+private fun StartScreenPrev() {
+    TrainDriverTheme {
+        CustomTextField(
+            "Номер телефона",
+            mutableStateOf("+7"),
+            LocaleState.RU.transformedNumber,
+            LocaleState.RU.maxLength()
+        )
+    }
 }
